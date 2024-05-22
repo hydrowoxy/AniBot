a7ede200b676ef435c39fc28b68dfa619e16289d
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-05-22 19:24:31 -0400

    renaming

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,9 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}`);
+        client.user.setPresence({
+            activities: [{ name: 'anime', type: 'WATCHING' }],
+            status: 'online'
+        });
     },
 }
